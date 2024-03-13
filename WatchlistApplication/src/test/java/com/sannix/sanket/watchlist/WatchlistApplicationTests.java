de81596c1ca1e2567de36d85d8a76262d80b27df
Sanket Anand <100087375+SanketAnand9693@users.noreply.github.com>
2024-03-14 02:04:14 +0530

    Watchlist Application
    Enternal API integration(omdb api), custom annotation, template engine(Thymeleaf), Database(MySQL) are used

---
+++ WatchlistApplication/src/test/java/com/sannix/sanket/watchlist/WatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sannix.sanket.watchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
